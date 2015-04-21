62dc4f6acea5ee1fbf8b1aa7544c26580c30f688
jhomikel <jhomikel@gmail.com>
2015-04-21 08:31:03 -0600

    Inicializando proyecto

---
+++ EjPlanilla/src/uml/Planilla.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package uml;
+
+/**
+ *
+ * @author PCVirtual
+ */
+public class Planilla {
+    private int id;
+    private String mes;
+
+    public Planilla() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMes() {
+        return mes;
+    }
+
+    public void setMes(String mes) {
+        this.mes = mes;
+    }
+    
+}
