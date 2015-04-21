20a90f2f2a2846f07904c7fc3c761318a5bb2715
equipo <equipo@MegaBacteria>
2015-04-21 08:44:07 -0600

    Agrego clase empleado

---
+++ EjPlanilla/src/uml/Empleado.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uml;
+
+/**
+ *
+ * @author equipo
+ */
+public class Empleado {
+    private int carnet;
+    private String nombre;
+    private double sueldo;
+
+    public Empleado() {
+    }
+
+    public int getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(int carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+    
+    
+    
+    
+}
